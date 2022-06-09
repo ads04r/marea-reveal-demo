a0d969d5eedaeecf160dc8aa7b0b6042d6b7430b
Ash <ash@drashsmith.com>
2022-06-09 10:37:37 +0000

    Added the map script

---
+++ map.php
@@ -0,0 +1,65 @@
+<?php
+
+$file = "";
+if(array_key_exists("person", $_GET))
+{
+	$file = "data/" . $_GET['person'] . "_export_results.json";
+	if(!(file_exists($file))) { $file = ""; }
+}
+
+if(strlen($file) == 0) { $file = "data/crystal_export_results.json"; }
+
+$data = json_decode(file_get_contents($file), true);
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>GeoJSON tutorial - Leaflet</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
+
+	<style>
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		.leaflet-container {
+			height: 400px;
+			width: 600px;
+			max-width: 100%;
+			max-height: 100%;
+		}
+	</style>
+
+	
+</head>
+<body>
+
+<div id='map' style="width: 100%; height: 100%;"></div>
+
+<script>
+	var data = <?php print(json_encode($data)); ?> ;
+
+	var map = L.map('map', {center: [50.9, -1.0], zoom: 2});
+
+	var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	var layer = L.geoJSON(data).addTo(map);
+
+	map.fitBounds(layer.getBounds());
+
+</script>
+
+
+
+</body>
+</html>
